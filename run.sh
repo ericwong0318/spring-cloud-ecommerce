bef2e1151a4bb3e938460802985cac88b39e17fc
Eric Wong <ericwong0318@gmail.com>
2025-06-06 12:27:57 +0800

    build: update project starting script

--- run.sh
+++ run.sh
@@ -1,40 +1,14 @@
 #!/bin/bash
 
-# Function to check if a port is in use
-is_port_in_use() {
-    local port=$1
-    lsof -i :$port -sTCP:LISTEN -t
-}
-
-# Function to stop process running on a port
-stop_process_on_port() {
-    local port=$1
-    local pid=$(is_port_in_use $port)
-    if [ -n "$pid" ]; then
-        echo "Stopping process running on port $port"
-        kill -9 $pid
-        while lsof -i :$port -sTCP:LISTEN -t >/dev/null; do
-            sleep 1
-        done
-    fi
-}
-
-# Stop if ports are in use
-stop_process_on_port 8761  # Eureka Server default port
-stop_process_on_port 8081  # Employee application default port
-stop_process_on_port 8082  # Product application default port
-
-$stop_process_on_port
-
  Start Eureka Server
 cd ~/spring-cloud-project/eureka-server
-./mvnw spring-boot:run &
-
-# Start Employee application
-cd ~/spring-cloud-project/employee
 ./mvnw spring-boot:run &
 
 # Start Product application
 cd ~/spring-cloud-project/product
 ./mvnw spring-boot:run &
+
+## Start API gateway
+#cd ~/spring-cloud-project/gateway/
+#./mvnw spring-boot:run &
 
