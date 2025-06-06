b188b5d658330d3270e0337d50e7164682723c59
Eric Wong <ericwong0318@gmail.com>
2025-06-06 12:27:10 +0800

    feat: add jakarta.persistence to Product

--- product/src/main/java/com/example/product/Product.java
+++ product/src/main/java/com/example/product/Product.java
@@ -1,9 +1,18 @@
 package com.example.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Entity
 public class Product {
-    public int productId;
-    public double price;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+    private double price;
+
+    public Product() {}
 
     public Product(double price, int productId) {
         this.price = price;
