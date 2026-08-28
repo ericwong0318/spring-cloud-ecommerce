50e1f6c10cfe425658bab39e6a6d64e3c73d44be
Eric Wong <ericwong0318@gmail.com>
2026-08-28 09:37:50 +0800

    feat: add eureka-server service discovery and Dockerfile

--- eureka-server/src/main/java/com/example/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eureka_server/EurekaServerApplication.java
@@ -1,15 +1,30 @@
 package com.example.eureka_server;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    @Bean
+    public OpenAPI eurekaOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Eureka Service Discovery")
+                        .version("1.0.0")
+                        .description("Netflix Eureka Server for service discovery")
+                        .contact(new Contact()
+                                .name("Spring Cloud Platform")
+                                .email("support@example.com")));
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
